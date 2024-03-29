9da2f08d5bea6fc1182c082b96d91d2afccfa9d2
Saikat Karmakar <saikickkarma@gmail.com>
2024-03-29 21:39:09 +0530

    exposed classes & methods in __init__.py

--- src/bmt_py/__init__.py
+++ src/bmt_py/__init__.py
@@ -1,22 +1,45 @@
 """Binary Merkle Tree operations on data"""
 
+from bmt_py.chunk import Chunk, ChunkAddress, make_chunk, root_hash_from_inclusion_proof
+from bmt_py.file import (
+    ChunkedFile,
+    ChunkInclusionProof,
+    file_address_from_inclusion_proof,
+    file_inclusion_proof_bottom_up,
+    get_bmt_index_of_segment,
+    make_chunked_file,
+)
+from bmt_py.span import get_span_value, make_span
+from bmt_py.utils import (
+    FlexBytes,
+    assert_flex_bytes,
+    bytes_equal,
+    bytes_to_hex,
+    is_flex_bytes,
+    keccak256_hash,
+    serialize_bytes,
+)
+
+__all__ = [
+    "Chunk",
+    "ChunkAddress",
+    "make_chunk",
+    "root_hash_from_inclusion_proof",
+    "ChunkedFile",
+    "ChunkInclusionProof",
+    "file_address_from_inclusion_proof",
+    "file_inclusion_proof_bottom_up",
+    "get_bmt_index_of_segment",
+    "make_chunked_file",
+    "FlexBytes",
+    "assert_flex_bytes",
+    "bytes_equal",
+    "bytes_to_hex",
+    "is_flex_bytes",
+    "keccak256_hash",
+    "serialize_bytes",
+    "get_span_value",
+    "make_span",
+]
+
 __version__ = "0.0.1"
-
-
-def add(a: int, b: int) -> int:
-    """
-    Add two integers.
-
-    Args:
-        a:
-            The first operand.
-        b:
-            The second operand.
-
-    Examples:
-        Add two integers
-
-            r = add(2, 3)
-            print(r)  # 5
-    """
-    return a + b
